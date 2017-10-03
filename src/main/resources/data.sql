8f41f1b700c447544b976fbe5e3055e459ab9c64
uniqueVitgen <vitgen1997@mail.ru>
2017-10-03 21:00:26 +0300

    3 commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into app_user(id,email,username,password) values (1,'admin@mail.ru','admin','admin');
-insert into app_user(id,email,username,password) values (2,'user@mail.ru','user','user');
-
-insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
-insert into app_user_roles(app_user_id,roles) values (1,'USER');
-insert into app_user_roles(app_user_id,roles) values (2,'USER');
+-- insert into app_user(id,email,username,password) values (1,'admin@mail.ru','admin','admin');
+-- insert into app_user(id,email,username,password) values (2,'user@mail.ru','user','user');
+--
+-- insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
+-- insert into app_user_roles(app_user_id,roles) values (1,'USER');
+-- insert into app_user_roles(app_user_id,roles) values (2,'USER');
 select CURRENT_DATE
